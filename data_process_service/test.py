4938b3219bb8df028329a401948356342c91f2a3
Davy Morgan <davy.morgan@aliyun.com>
2019-07-28 14:03:22 +0800

    test cffilter

--- data_process_service/test.py
+++ data_process_service/test.py
@@ -7,6 +7,8 @@
 from absl import app
 from absl import flags
 
+import pandas as pd
+
 import loader as LOADER
 import filter as FILTER
 
@@ -35,13 +37,13 @@ def test_csvloader(flags_obj):
 
 def test_cffilter(flags_obj):
 
-    record = test_csvloader(flags_obj)
+    record = pd.DataFrame({'uid': [1,2,2,3,3,3,4,4,4,4], 'iid': [1,2,2,3,3,3,4,4,4,4]})
     filter = FILTER.CFFilter(flags_obj, record)
-
+    print(record.head(10))
     print('{} records before filter!'.format(len(record)))
 
-    record = filter.filter_user_k_core(record, 10)
-
+    record = filter.filter_user_k_core(record, 2)
+    print(record.head(10))
     print('{} records after filter!'.format(len(record)))
 
 
