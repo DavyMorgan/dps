133dc6633b2ea8bfe22dd102e0522cc2a87d23bd
Davy Morgan <davy.morgan@aliyun.com>
2019-07-28 13:40:40 +0800

    test cffilter

--- data_process_service/test.py
+++ data_process_service/test.py
@@ -8,11 +8,13 @@ from absl import app
 from absl import flags
 
 import loader as LOADER
+import filter as FILTER
 
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('name', 'test', 'Test name.')
+flags.DEFINE_bool('test', True, 'Whether in test mode.')
 flags.DEFINE_string('load_path', '', 'Path to load file.')
 flags.DEFINE_string('save_path', '', 'Path to save file.')
 
@@ -28,12 +30,27 @@ def test_csvloader(flags_obj):
     loader.load_file(filename, argu_dict)
     loader.record.info()
 
+    return loader.record
+
+
+def test_cffilter(flags_obj):
+
+    record = test_csvloader(flags_obj)
+    filter = FILTER.CFFilter(flags_obj, record)
+
+    print('{} records before filter!'.format(len(record)))
+
+    record = filter.filter_user_k_core(record, 10)
+
+    print('{} records after filter!'.format(len(record)))
+
 
 def main(argv):
 
     flags_obj = flags.FLAGS
 
-    test_csvloader(flags_obj)
+    #test_csvloader(flags_obj)
+    test_cffilter(flags_obj)
 
 
 if __name__ == "__main__":
