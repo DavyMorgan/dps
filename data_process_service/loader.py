566060563cca78eb332d961d79cbb8514af6030d
Davy Morgan <davy.morgan@aliyun.com>
2019-07-28 13:02:59 +0800

    loader csvloader

---
+++ data_process_service/loader.py
@@ -0,0 +1,42 @@
+#!/usr/local/anaconda3/envs/torch-1.0-py3/bin/python
+#coding=utf-8
+#pylint: disable=no-member
+#pylint: disable=no-name-in-module
+#pylint: disable=import-error
+
+import pandas as pd
+
+import os
+
+
+class Loader(object):
+
+    def __init__(self, flags_obj):
+
+        self.name = flags_obj.name
+        self.load_path = flags_obj.load_path
+        self.save_path = flags_obj.save_path
+        self.check_save_path()
+    
+    def check_save_path(self):
+
+        if not os.path.exists(self.save_path):
+
+            os.mkdir(self.save_path)
+    
+    def load_file(self, filename, argu_dict):
+
+        pass
+
+
+class CSVLoader(Loader):
+
+    def __init__(self, flags_obj):
+
+        super(CSVLoader, self).__init__(flags_obj)
+    
+    def load_file(self, filename, argu_dict):
+
+        argu_dict['filepath_or_buffer'] = filename
+        self.record = pd.read_csv(**argu_dict)
+
