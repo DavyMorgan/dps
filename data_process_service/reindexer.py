fd24dcfe8f9a832ca82b344cf562fb4629d80b35
Davy Morgan <davy.morgan@aliyun.com>
2019-07-28 14:13:28 +0800

    reindexer

---
+++ data_process_service/reindexer.py
@@ -0,0 +1,30 @@
+#!/usr/local/anaconda3/envs/torch-1.0-py3/bin/python
+#coding=utf-8
+#pylint: disable=no-member
+#pylint: disable=no-name-in-module
+#pylint: disable=import-error
+
+import pandas as pd
+
+
+class Reindexer(object):
+
+    def __init__(self, flags_obj):
+
+        self.name = flags_obj.name + '_reindexer'
+    
+    def reindex_core(self, record, column_name):
+
+        keys = record[column_name].unique()
+        num_entities = record[column_name].nunique()
+        reindex_map = {keys[i]: i for i in range(num_entities)}
+
+        record[column_name] = record[column_name].map(reindex_map)
+    
+    def reindex_user(self, record):
+
+        self.reindex_core(record, 'uid')
+    
+    def reindex_item(self, record):
+
+        self.reindex_core(record, 'iid')
