7991915d5d679c6a132d4f207cc5466c7b258094
Davy Morgan <davy.morgan@aliyun.com>
2019-07-29 16:56:45 +0800

    test splitter

--- data_process_service/test.py
+++ data_process_service/test.py
@@ -12,6 +12,7 @@ import pandas as pd
 import loader as LOADER
 import rek_filter as FILTER
 import reindexer as REINDEXER
+import splitter as SPLITTER
 
 
 FLAGS = flags.FLAGS
@@ -62,13 +63,58 @@ def test_reindexer(flags_obj):
     print('data frames after reindex!')
 
 
+def test_absolutesplitter(flags_obj):
+
+    #record = pd.DataFrame({'uid': [3,3,3,4,4,4,4], 'iid': [1,2,3,4,5,6,7], 'ts': [1,2,3,4,5,6,7]})
+    record = pd.DataFrame({'uid': [4,4,4,4,2,2], 'iid': [10,9,8,7,6,5], 'ts': [6,5,4,3,2,1]})
+    splitter = SPLITTER.AbsoluteSplitter(flags_obj, record)
+    print(record.head(10))
+    print('data frames before split!')
+
+    record = record.sample(frac=1.0)
+    splits = [-1, 1, 1]
+    splitter.split(record, splits)
+
+    print(splitter.train_record.head(10))
+    print('train split!')
+
+    print(splitter.val_record.head(10))
+    print('val split!')
+
+    print(splitter.test_record.head(10))
+    print('test split!')
+
+
+def test_percentagesplitter(flags_obj):
+
+    record = pd.DataFrame({'uid': [2,2,4,4,4,4], 'iid': [10,9,8,7,6,5], 'ts': [6,5,4,3,2,1]})
+    #record = pd.DataFrame({'uid': [4,4,4,4,4,2,2], 'iid': [10,9,8,7,6,5,4], 'ts': [6,6,6,6,6,6,6]})
+    splitter = SPLITTER.PercentageSplitter(flags_obj, record)
+    print(record.head(10))
+    print('data frames before split!')
+
+    record = record.sample(frac=1.0)
+    splits = [0.6,0.2,0.2]
+    splitter.split(record, splits)
+
+    print(splitter.train_record.head(10))
+    print('train split!')
+
+    print(splitter.val_record.head(10))
+    print('val split!')
+
+    print(splitter.test_record.head(10))
+    print('test split!')
+
 def main(argv):
 
     flags_obj = flags.FLAGS
 
     #test_csvloader(flags_obj)
     #test_cffilter(flags_obj)
-    test_reindexer(flags_obj)
+    #test_reindexer(flags_obj)
+    #test_absolutesplitter(flags_obj)
+    test_percentagesplitter(flags_obj)
 
 
 if __name__ == "__main__":
