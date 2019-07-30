edccf29d2c05bfee0930ad7b5efd47e1b76bfbb9
Davy Morgan <davy.morgan@aliyun.com>
2019-07-30 19:00:58 +0800

    SparseGenerator Coo/Lil/Dok Generator

--- data_process_service/generator.py
+++ data_process_service/generator.py
@@ -26,7 +26,42 @@ class Generator(object):
         self.n_item = record['iid'].nunique()
 
 
-class LilGenerator(Generator):
+class SparseGenerator(Generator):
+
+    def __init__(self, flags_obj):
+
+        super(SparseGenerator, self).__init__(flags_obj)
+    
+    def generate(self, record):
+
+        raise NotImplementedError
+    
+    def generate_coo(self, record):
+
+        self.stat(record)
+
+        values = np.ones(self.n_record)
+        users = record['uid'].to_numpy()
+        items = record['iid'].to_numpy()
+        coo_record = sp.coo_matrix((values, (users, items)), shape=(self.n_user, self.n_item))
+
+        return coo_record
+
+
+class CooGenerator(SparseGenerator):
+
+    def __init__(self, flags_obj):
+
+        super(CooGenerator, self).__init__(flags_obj)
+
+    def generate(self, record):
+
+        coo_record = self.generate_coo(record)
+
+        return coo_record
+
+
+class LilGenerator(SparseGenerator):
 
     def __init__(self, flags_obj):
 
@@ -34,11 +69,19 @@ class LilGenerator(Generator):
     
     def generate(self, record):
 
-        self.stat(record)
-
-        values = np.ones(self.n_record)
-        users = record['uid'].to_numpy()
-        items = record['iid'].to_numpy()
-        lil_record = sp.coo_matrix((values, (users, items)), shape=(self.n_user, self.n_item)).tolil()
+        lil_record = self.generate_coo(record).tolil()
 
         return lil_record
+
+
+class DokGenerator(SparseGenerator):
+
+    def __init__(self, flags_obj):
+
+        super(DokGenerator, self).__init__(flags_obj)
+    
+    def generate(self, record):
+
+        dok_record = self.generate_coo(record).todok()
+
+        return dok_record
