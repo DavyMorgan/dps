78954828427a7ecd8b2d9f72a0418ff7d06e4734
Davy Morgan <davy.morgan@aliyun.com>
2019-07-30 20:14:29 +0800

    COOSaver

---
+++ data_process_service/saver.py
@@ -0,0 +1,43 @@
+#!/usr/local/anaconda3/envs/torch-1.0-py3/bin/python
+#coding=utf-8
+#pylint: disable=no-member
+#pylint: disable=no-name-in-module
+#pylint: disable=import-error
+
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+
+import os
+
+
+class Saver(object):
+
+    def __init__(self, flags_obj):
+
+        self.name = flags_obj.name + '_saver'
+        self.save_path = flags_obj.save_path
+        if not flags_obj.test:
+            self.check_save_path()
+    
+    def check_save_path(self):
+
+        if not os.path.exists(self.save_path):
+
+            os.mkdir(self.save_path)
+    
+    def save_file(self, filename, data):
+
+        raise NotImplementedError
+
+
+class COOSaver(Saver):
+
+    def __init__(self, flags_obj):
+
+        super(COOSaver, self).__init__(flags_obj)
+    
+    def save_file(self, filename, data):
+
+        filename = os.path.join(self.save_path, filename)
+        sp.save_npz(filename, data)
